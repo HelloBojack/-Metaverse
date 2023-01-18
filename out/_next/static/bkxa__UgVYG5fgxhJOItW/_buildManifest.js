e8d4bc932337d47e8cbf0a9f9303364a1604805d
mi_lekang.xu <xlk970115@gmail.com>
2023-01-18 14:45:09 +0800

    Deploy

--- out/_next/static/bkxa__UgVYG5fgxhJOItW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/910-2c43cb349485e376.js","static/chunks/pages/index-de8f66912eda7da5.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
